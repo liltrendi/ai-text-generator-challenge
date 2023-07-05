a33fcf31ea31e3a7ec5d716835eabe48d962200a
Brian Njogu <briancanspit@gmail.com>
2023-07-05 17:43:54 +0300

    fix: tailor bot responses from settings

--- src/hooks/useAuth/index.tsx
+++ src/hooks/useAuth/index.tsx
@@ -1,36 +1,16 @@
 import { useEffect, useState } from "react";
-// import { usePathname } from "next/navigation";
 import { User } from "gotrue-js";
 import { getCurrentUser } from "@/services/auth";
 
 export const useAuth = () => {
-    // const router = useRouter();
-    // const pathname = usePathname();
-
     const [loading, setLoading] = useState<boolean>(true);
     const [user, setUser] = useState<null | undefined | User>(undefined);
-
-    // const redirect = useCallback(
-    //     (currentUser: null | undefined | User) => {
-    // if (currentUser) {
-    //     router.push("/");
-    //     return;
-    // }
-
-    // if (pathname === "/") {
-    //     router.push("/login");
-    // }
-    //     },
-    //     [pathname]
-    // );
 
     useEffect(() => {
         const currentUser = getCurrentUser();
 
         setUser(currentUser);
         setLoading(false);
-
-        // redirect(currentUser);
     }, []);
 
     return { loading, user };
