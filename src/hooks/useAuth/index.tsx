6931bf706357d5f49ed58485eec0ffec5abd9e3b
Brian Njogu <briancanspit@gmail.com>
2023-07-05 06:03:58 +0300

    fix: auth

--- src/hooks/useAuth/index.tsx
+++ src/hooks/useAuth/index.tsx
@@ -1,28 +1,14 @@
 import { useEffect, useState } from "react";
-// import { useRouter, usePathname } from "next/navigation";
+import { useRouter, usePathname } from "next/navigation";
 import { User } from "gotrue-js";
 import { getCurrentUser } from "@/services/auth";
 
 export const useAuth = () => {
-    // const router = useRouter();
-    // const pathname = usePathname();
+    const router = useRouter();
+    const pathname = usePathname();
 
     const [loading, setLoading] = useState<boolean>(true);
     const [user, setUser] = useState<null | undefined | User>(undefined);
-
-    // const redirect = useCallback(
-    //     (currentUser: null | undefined | User) => {
-    //         if (currentUser) {
-    //             router.replace("/");
-    //             return;
-    //         }
-
-    //         if (pathname === "/") {
-    //             router.replace("/login");
-    //         }
-    //     },
-    //     [pathname]
-    // );
 
     useEffect(() => {
         const currentUser = getCurrentUser();
@@ -30,7 +16,11 @@ export const useAuth = () => {
         setUser(currentUser);
         setLoading(false);
 
-        // redirect(currentUser);
+        if (currentUser) {
+            router.push("/");
+        } else if (!currentUser && pathname === "/") {
+            router.push("/login");
+        }
     }, []);
 
     return { loading, user };
