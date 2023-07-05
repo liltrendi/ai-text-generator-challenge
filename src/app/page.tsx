9311d6e8946b1809411d26f9ce4b08ee4b913dbf
Brian Njogu <briancanspit@gmail.com>
2023-07-05 06:16:54 +0300

    fix: redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,19 @@ import { AppContainer } from "@/app/styles";
 import Chats from "@/components/chats";
 import Prompt from "@/components/prompt";
 import { useChatHistory } from "@/hooks/useChatHistory";
+import { getCurrentUser } from "@/services/auth";
+import { useRouter } from "next/navigation";
 
 const Home = () => {
+    const router = useRouter();
     const { chats, containerRef, scrollToBottom, appendToStatefulChatHistory } =
         useChatHistory();
+
+    const user = getCurrentUser();
+    if (!user) {
+        router.push("/login");
+        return undefined;
+    }
 
     return (
         <AppContainer>
